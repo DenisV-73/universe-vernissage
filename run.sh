7dfb480a3821a8b05f1c5ae5249d089c46cf61ea
ibrahim <ibrahim@md-shop.lan>
2023-05-08 19:01:13 +0000

    Mon May  8 19:01:13 UTC 2023

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
-#docker rm dockerize-vuejs-app -f
-#docker rm anasse-web -f
+#docker rm dockerize-vuejs-app-1 -f
 
 docker build -t vuejs-cookbook/dockerize-vuejs-app .
 #
-docker run -d -it -p 8080:80 --rm --name dockerize-vuejs-app-1 vuejs-cookbook/dockerize-vuejs-app
+docker run -d -it -p 7791:80 --rm --name dockerize-vuejs-app-1 vuejs-cookbook/dockerize-vuejs-app
 #
