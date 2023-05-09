fef8a28112a9942708b1dd0a41d30dcf9f3d41c2
ibrahim <ibrahim@jenkins.skyline.lan>
2023-05-09 10:42:55 +0000

    Tue May  9 10:42:55 UTC 2023

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ docker build -t vuejs-cookbook/dockerize-vuejs-app2 .
 #docker run -d -it -p 7791:80 --rm --name dockerize-vuejs-app-1 vuejs-cookbook/dockerize-vuejs-app
 docker run -d -it -p 7792:80 --rm --name dockerize-vuejs-app-2 vuejs-cookbook/dockerize-vuejs-app2
 #
+#
